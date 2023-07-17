caf96947a1fa61fd349a7311521995e7176c248e
shahzeb.khan <shahzeb.khan@infosys.com>
2023-07-17 10:56:09 +0530

    logic of black jack

--- 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
@@ -2,3 +2,17 @@
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
+const firstCard = 21
+const secondCard = 9
+
+sum = firstCard+secondCard
+
+if(sum<21){
+    console.log("You can hit.")
+}
+else if(sum===21){
+    console.log("Blackjack!")
+}
+else if(sum>21){
+    console.log(`Bust! You have ${firstCard + "+" + secondCard}`)
+}
